3d2c09e0233d901ef2e4bce1fb6c71f831b83cb0
Nicolau Riess <nicolauriess@gmail.com>
2022-03-31 11:23:03 -0300

    fixing call to function

--- src/api/set-stake.ts
+++ src/api/set-stake.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import { Address, ApiResult, ProjectId, Tokens } from '../types/types';
 
-export const setStake = async (address: Address, projectId: ProjectId, tokensId: Tokens): ApiResult<void> => {
+export const setStake = async (address: Address, projectId: ProjectId, tokens: Tokens): ApiResult<void> => {
     try {
         await axios.post(
-            process.env.SERVER + '/set-staked',
-            { address, projectId, tokensId },
+            process.env.SERVER + '/users/set-staked',
+            { address, projectId, tokens },
             {
                 headers: {
                     authentication:
