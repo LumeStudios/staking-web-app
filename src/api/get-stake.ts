6bf829a89630f68d87449d1b224a0e1f107d9abf
Nicolau Riess <nicolauriess@gmail.com>
2022-04-22 11:29:34 -0300

    changes

--- src/api/get-stake.ts
+++ src/api/get-stake.ts
@@ -4,9 +4,8 @@ import { Address, ApiResult, ProjectId, Tokens } from "../types/types";
 export const getStake = async (address: Address, projectId: ProjectId): ApiResult<{ tokens: Tokens }> => {
     try {
         const response = await axios.get(
-            process.env.SERVER + '/users/get-staked',
+            process.env.SERVER + `/users/get-staked?address=${address}&projectId=${projectId}`,
             {
-                params: { address, projectId },
                 headers: {
                     authentication:
                         process.env.AUTHENTICATION as string,
